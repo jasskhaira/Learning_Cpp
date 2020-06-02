dccc8bc8683ca8fbcefe4aec8d94fe95f4f342fc
Jaspreet <jaspreetsinghkhaira@gmail.com>
2020-06-02 04:46:01 -0400

    arranging Stuff

--- Algorithms/Interpolation Search/src/Interpolation Search.cpp
+++ Algorithms/Interpolation Search/src/Interpolation Search.cpp
@@ -9,7 +9,44 @@
 #include <iostream>
 using namespace std;
 
+int Inertpol_Search(int *arr , int size , int var){
+	int lo = 0 , hi = size -1 , mid = -1 , i  ;
+
+	while(lo<= arr[i] ){
+		if( lo == hi || arr[lo] == arr [hi])
+			return(-1);
+
+		mid = lo + ((hi - lo) / (arr[hi] - arr[lo])) * (var - arr[lo]);
+
+		if(arr[mid] == var)
+			return(mid);
+
+		else{
+
+			if (arr[mid] < var)
+				lo = mid+1;
+
+			else if (arr[mid] > var )
+				hi = mid -1;
+
+
+		}
+
+	}
+
+}
+
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int a[] = { 10 , 20 , 30 , 33 , 34 ,40 , 50 , 60 , 70 , 99};
+
+
+	int loc;
+
+	loc = Inertpol_Search( a , 10 , 34);
+
+	cout<<" Location of 34 "<<loc;
+
+
 	return 0;
 }
